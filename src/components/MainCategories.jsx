3b927982b69b00526b9cdd1d6e9ab0b57fe80efb
rockyroed <roed-imp.vo97b@slmail.me>
2025-03-25 16:58:58 +0800

    Use Search component

--- src/components/MainCategories.jsx
+++ src/components/MainCategories.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import Search from "./Search";
 
 const MainCategories = () => {
   return (
@@ -44,24 +45,7 @@ const MainCategories = () => {
       </div>
       <span className="text-xl font-medium">|</span>
       {/* SEARCH */}
-      <div className="bg-gray-100 p-2 rounded-full flex items-center gap-2">
-        <svg
-          width="20"
-          height="20"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z"
-            stroke="#000000"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-        <input type="text" placeholder="Search a post..." className="bg-transparent focus:outline-none" />
-      </div>
+      <Search />
     </div>
   );
 };
