0d2f5f478c48d689067afa2b7d3090acbbf538b0
rockyroed <roed-imp.vo97b@slmail.me>
2025-03-26 12:46:09 +0800

    Add error handling

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,14 @@ app.use("/users", userRouter);
 app.use("/posts", postRouter);
 app.use("/comments", commentRouter);
 
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    message: error.message,
+    status: error.status,
+    stack: error.stack,
+  });
+});
+
 app.listen(3000, async () => {
   await connectDB();
   console.log("Server running on port 3000");
