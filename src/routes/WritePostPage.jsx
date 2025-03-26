018a1e87715fef58e72b07a75dfcd2913ef75597
rockyroed <roed-imp.vo97b@slmail.me>
2025-03-26 10:50:26 +0800

    Update WritePostPage.jsx

--- src/routes/WritePostPage.jsx
+++ src/routes/WritePostPage.jsx
@@ -1,9 +1,41 @@
+import { useUser } from "@clerk/clerk-react";
+import 'react-quill-new/dist/quill.snow.css';
+import ReactQuill from "react-quill-new";
+
 const WritePostPage = () => {
+  const { isLoaded, isSignedIn } = useUser();
+
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+
+  if (isLoaded && !isSignedIn) {
+    return <div>Access denied. You must be logged in.</div>;
+  }
+
   return (
-    <div>
-      <h1>Write Post Page</h1>
+    <div className="h-[calc(100vh-64px)] md:h-[calc(100vh-80px)] flex flex-col gap-6">
+      <h1 className="text-xl font-light">Create a New Post</h1>
+      <form className="flex flex-col gap-6 flex-1 mb-6">
+        <button className="w-max p-2 shadow-md rounded-xl text-sm text-gray-500 bg-white">Add a cover image</button>
+        <input className="text-4xl font-semibold bg-transparent outline-none" type="text" placeholder="Post title" />
+        <div className="flex items-center gap-4">
+          <label className="text-sm">Choose a category:</label>
+          <select className="p-2 rounded-xl bg-white shadow-md outline-none" name="category">
+            <option value="general">General</option>
+            <option value="web-design">Web Design</option>
+            <option value="development">Development</option>
+            <option value="databases">Databases</option>
+            <option value="search-engines">Search Engines</option>
+            <option value="marketing">Marketing</option>
+          </select>
+        </div>
+        <textarea className="p-4 rounded-xl bg-white shadow-md outline-none" name="description" placeholder="A short description" />
+        <ReactQuill theme="snow" className="flex-1 rounded-xl bg-white shadow-md" />
+        <button className="bg-blue-800 text-white font-medium rounded-xl mt-4 p-2 w-36">Send</button>
+      </form>
     </div>
   );
-}
+};
 
 export default WritePostPage;
