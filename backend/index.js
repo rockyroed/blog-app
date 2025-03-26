7398d10ec2aa4390a146f1468432f3ed14a4260a
rockyroed <roed-imp.vo97b@slmail.me>
2025-03-26 15:09:03 +0800

    Use webhook router

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,14 @@ import express from "express";
 import userRouter from "./routes/user.route.js";
 import postRouter from "./routes/post.route.js";
 import commentRouter from "./routes/comment.route.js";
+import webhookRouter from "./routes/webhook.route.js";
 import connectDB from "./lib/connectDB.js";
 
 const app = express();
 
-app.use(express.json());
+app.use("/webhooks", webhookRouter);
 
+app.use(express.json());
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
 app.use("/comments", commentRouter);
