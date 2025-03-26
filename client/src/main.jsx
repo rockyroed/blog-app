9e07201fbac846c8c643682238d5c111ee0e2f9a
rockyroed <roed-imp.vo97b@slmail.me>
2025-03-26 16:06:46 +0800

    Add QueryClientProvider to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -16,6 +16,9 @@ import SinglePostPage from "./routes/SinglePostPage.jsx";
 import WritePostPage from "./routes/WritePostPage.jsx";
 import MainLayout from "./layouts/MainLayout.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -58,7 +61,9 @@ const router = createBrowserRouter([
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ClerkProvider>
   </StrictMode>
 );
