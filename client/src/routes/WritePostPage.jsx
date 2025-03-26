61732bffbca791b8296eedaa0da93a88fd775a9e
rockyroed <roed-imp.vo97b@slmail.me>
2025-03-26 16:08:53 +0800

    Add name values to form elements

--- client/src/routes/WritePostPage.jsx
+++ client/src/routes/WritePostPage.jsx
@@ -16,12 +16,22 @@ const WritePostPage = () => {
   return (
     <div className="h-[calc(100vh-64px)] md:h-[calc(100vh-80px)] flex flex-col gap-6">
       <h1 className="text-xl font-light">Create a New Post</h1>
-      <form className="flex flex-col gap-6 flex-1 mb-6">
-        <button className="w-max p-2 shadow-md rounded-xl text-sm text-gray-500 bg-white">Add a cover image</button>
-        <input className="text-4xl font-semibold bg-transparent outline-none" type="text" placeholder="Post title" />
+      <form onSubmit={handleSubmit} className="flex flex-col gap-6 flex-1 mb-6">
+        <button className="w-max p-2 shadow-md rounded-xl text-sm text-gray-500 bg-white">
+          Add a cover image
+        </button>
+        <input
+          className="text-4xl font-semibold bg-transparent outline-none"
+          type="text"
+          placeholder="Post title"
+          name="title"
+        />
         <div className="flex items-center gap-4">
           <label className="text-sm">Choose a category:</label>
-          <select className="p-2 rounded-xl bg-white shadow-md outline-none" name="category">
+          <select
+            className="p-2 rounded-xl bg-white shadow-md outline-none"
+            name="category"
+          >
             <option value="general">General</option>
             <option value="web-design">Web Design</option>
             <option value="development">Development</option>
@@ -30,9 +40,21 @@ const WritePostPage = () => {
             <option value="marketing">Marketing</option>
           </select>
         </div>
-        <textarea className="p-4 rounded-xl bg-white shadow-md outline-none" name="description" placeholder="A short description" />
-        <ReactQuill theme="snow" className="flex-1 rounded-xl bg-white shadow-md" />
-        <button className="bg-blue-800 text-white font-medium rounded-xl mt-4 p-2 w-36">Send</button>
+        <textarea
+          className="p-4 rounded-xl bg-white shadow-md outline-none"
+          name="description"
+          placeholder="A short description"
+        />
+        <ReactQuill
+          theme="snow"
+          className="flex-1 rounded-xl bg-white shadow-md"
+          name="content"
+          value={value}
+          onChange={setValue}
+        />
+        <button className="bg-blue-800 text-white font-medium rounded-xl mt-4 p-2 w-36">
+          Send
+        </button>
       </form>
     </div>
   );
