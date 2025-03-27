0139ccd104f71fb743a1ef5589483d1d90a269f9
rockyroed <roed-imp.vo97b@slmail.me>
2025-03-27 15:07:20 +0800

    Add more access control headers

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,14 @@ import cors from "cors";
 
 const app = express();
 app.use(cors(process.env.CLIENT_URL));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(clerkMiddleware());
 
 app.use("/webhooks", webhookRouter);
@@ -19,17 +27,17 @@ app.use("/posts", postRouter);
 app.use("/comments", commentRouter);
 
 app.get("/protect", requireAuth(), (req, res) => {
-  const {userId} = req.auth;
+  const { userId } = req.auth;
 
-  if(!userId) {
+  if (!userId) {
     return res.status(401).json({
-      message: "Unauthorized"
+      message: "Unauthorized",
     });
   }
 
   res.status(200).json({
     message: "Protected route",
-    userId
+    userId,
   });
 });
 
